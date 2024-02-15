9a3c444d23945fff62de49aae86b4f05d9eb2b94
claudiocello <65453770+claudiocello@users.noreply.github.com>
2024-02-15 15:43:48 +0100

    added new events

--- src/components.js/BandoButton.js
+++ src/components.js/BandoButton.js
@@ -1,21 +1,22 @@
-import iconL from "../images/Group 7.png"
-import iconR from "../images/Group 1.png"
+import iconL from "../images/Group 7.png";
+import iconR from "../images/Group 1.png";
 
-
-
-function BandoButton(){
-
-
-    return(
-        <>
-        <div className="bandobutton-section">
-            <img src={iconL} alt="logo" width="150" className="ludiL"/>
-            <button className="bando-button" ><a  class="link-style" href="/eventi/EnglishStrings" >Prossimo Concerto: English Strings 23/04/2023 </a></button>
-            <img src={iconR} alt="logo" width="150" className="ludiL"/>
-        </div>
-        <hr class="hr5"/> 
-        </>
-    )
+function BandoButton() {
+  return (
+    <>
+      <div className="bandobutton-section">
+        <img src={iconL} alt="logo" width="150" className="ludiL" />
+        <button className="bando-button">
+          <a class="link-style" href="/eventi/primer">
+            Prossimo Concerto: {"\n"} Primer - Duo Doulov-Pogosyan - Stagione da
+            Camera Ludi Sonores 15/02/2024{" "}
+          </a>
+        </button>
+        <img src={iconR} alt="logo" width="150" className="ludiL" />
+      </div>
+      <hr class="hr5" />
+    </>
+  );
 }
 
-export default BandoButton
+export default BandoButton;
